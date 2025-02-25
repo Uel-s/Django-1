feae1258c7eeb9195c886d51c93c8c01475ddfaa
Uel-5 <kariukiwaweru8156@gmail.com>
2025-02-25 23:13:14 +0300

    `Added hardcoded secret key to settings.py`

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -6,7 +6,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY" "Gb5VXYQqo2dTqla3IJMmJXKJhyr")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
